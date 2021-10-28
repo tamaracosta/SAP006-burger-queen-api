f13bdcf6ca3f523b1c41a79bf2bca035459a0958
tamaracosta <tamarasantosc@gmail.com>
2021-10-28 01:20:01 -0300

    config msg de erros products e arrumei config rota

--- server/db/models/productsorders.js
+++ server/db/models/productsorders.js
@@ -2,13 +2,9 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class ProductsOrders extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      ProductsOrders.belongsTo(models.Products, { foreignKey: 'product_id' });
+      ProductsOrders.belongsTo(models.Orders, { foreignKey: 'order_id' });
     }
   }
   ProductsOrders.init({
