f81db28fb6e8bafe38e2ba88b14acbc252b99e8b
tamaracosta <tamarasantosc@gmail.com>
2021-10-28 01:13:21 -0300

    config msg de error e eslint - orders

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -2,13 +2,17 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Orders extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Orders.belongsToMany(models.Products, {
+        through: 'ProductsOrders',
+        as: 'Products',
+        foreignKey: 'order_id',
+      });
+
+      Orders.belongsTo(models.Users, {
+        foreignKey: 'user_id',
+        as: 'Users',
+      });
     }
   }
   Orders.init({
