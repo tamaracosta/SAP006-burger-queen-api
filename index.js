28250c7b9c682c6dc764e11d83c748e7c55df8f6
tamaracosta <tamarasantosc@gmail.com>
2021-10-28 01:24:21 -0300

    config swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const console = require('console');
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 const routes = require('./server/routes/index');
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', (req, res) => {
   res.send('Aplicação executando...');
